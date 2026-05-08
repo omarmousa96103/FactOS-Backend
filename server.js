a878a65ef5e592b71382a7913b48a0c495a1452a
Omar Mousa <omarmousa96103@gmail.com>
2026-05-08 15:03:47 +0300

    fix: listen on 0.0.0.0 for railway

--- server.js
+++ server.js
@@ -35,4 +35,4 @@ app.get('/', (req, res) => {
 app.use(require('./middleware/errorMiddleware'))
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`))
