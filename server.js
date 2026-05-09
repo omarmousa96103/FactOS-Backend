52ebd09c18f4bf32cea2f2d2dde5f0a9fa1a3c69
Omar Mousa <omarmousa96103@gmail.com>
2026-05-09 19:47:09 +0300

    fix: remove app.options wildcard for express compatibility

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ const corsOptions = {
         'https://fact-os.vercel.app',
     ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }
 
 app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
 app.use(express.json())
 
 app.use('/api/auth',      require('./routes/authRoutes'))
